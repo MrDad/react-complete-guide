19cf5991cea1ffbaf93ba8a72860d60ac37e615b
Anthony <anthony@Anthonys-MacBook-Pro.local>
2021-12-26 17:08:23 -0500

    moved ExpenseItems into new Expenses component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Title1",
+      title: "Water",
       amount: 101,
       date: new Date(2021, 0, 1),
     },
     {
       id: "e2",
-      title: "Title2",
+      title: "Electricity",
       amount: 102,
       date: new Date(2021, 1, 1),
     },
     {
       id: "e3",
-      title: "Title3",
+      title: "Food",
       amount: 103,
       date: new Date(2021, 2, 1),
     },
     {
       id: "e4",
-      title: "Title4",
+      title: "Cell",
       amount: 104,
       date: new Date(2021, 3, 1),
     },
@@ -31,26 +31,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses}/>
     </div>
   );
 }
