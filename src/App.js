208455498034ce5d1dec3def18f86aad67aa1741
Anthony <anthony@Anthonys-MacBook-Pro.local>
2021-12-26 16:34:23 -0500

    ExpenseDate componnent creation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       id: "e1",
       title: "Title1",
       amount: 101,
-      date: new Date(2021, 1, 1),
+      date: new Date(2021, 0, 1),
     },
     {
       id: "e2",
@@ -18,13 +18,13 @@ function App() {
       id: "e3",
       title: "Title3",
       amount: 103,
-      date: new Date(2021, 1, 1),
+      date: new Date(2021, 2, 1),
     },
     {
       id: "e4",
       title: "Title4",
       amount: 104,
-      date: new Date(2021, 1, 1),
+      date: new Date(2021, 3, 1),
     },
   ];
 
@@ -35,22 +35,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
