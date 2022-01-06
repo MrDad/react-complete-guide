3f2176e809f86dda3b1c5fd7b171ed72ccfa7cc1
Anthony <ahaddadkc@gmail.com>
2022-01-05 22:39:32 -0500

    Update the Expenses list to dynamic updates

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMYEXPENSES = [
+  {
+    id: "e1",
+    title: "Water",
+    amount: 101,
+    date: new Date(2019, 0, 1),
+  },
+  {
+    id: "e2",
+    title: "Electricity",
+    amount: 102,
+    date: new Date(2020, 1, 1),
+  },
+  {
+    id: "e3",
+    title: "Food",
+    amount: 103,
+    date: new Date(2021, 2, 1),
+  },
+  {
+    id: "e4",
+    title: "Cell",
+    amount: 104,
+    date: new Date(2021, 3, 1),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Water",
-      amount: 101,
-      date: new Date(2021, 0, 1),
-    },
-    {
-      id: "e2",
-      title: "Electricity",
-      amount: 102,
-      date: new Date(2021, 1, 1),
-    },
-    {
-      id: "e3",
-      title: "Food",
-      amount: 103,
-      date: new Date(2021, 2, 1),
-    },
-    {
-      id: "e4",
-      title: "Cell",
-      amount: 104,
-      date: new Date(2021, 3, 1),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMYEXPENSES);
 
   const addExpenseHandler = (expense) =>{
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
